ef3633a09d836635e5c3dbb8921b107474f5a038
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2025-06-20 17:29:39 +0700

    Update Google and Microsoft login button UI

--- components/auth/MicrosoftLoginButton.tsx
+++ components/auth/MicrosoftLoginButton.tsx
@@ -16,13 +16,13 @@ export default function MicrosoftLoginButton() {
         <Button
             type="button"
             variant="outline"
-            className="w-full hover:border-blue-500 hover:bg-blue-50/40 focus:ring-blue-500 focus:ring-offset-blue-100"
+            className="w-full font-normal rounded-[5px] hover:border-blue-500 hover:bg-blue-50/40 focus:ring-blue-500 focus:ring-offset-blue-100"
             onClick={handleMicrosoftLogin}
         >
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
                 <path fill="#ff5722" d="M6 6H22V22H6z" transform="rotate(-180 14 14)"></path><path fill="#4caf50" d="M26 6H42V22H26z" transform="rotate(-180 34 14)"></path><path fill="#ffc107" d="M26 26H42V42H26z" transform="rotate(-180 34 34)"></path><path fill="#03a9f4" d="M6 26H22V42H6z" transform="rotate(-180 14 34)"></path>
             </svg>
-            Microsoft
+            Đăng nhập bằng Microsoft
         </Button>
     );
 }
